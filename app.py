982e65a761cee1f076224c4c2a7d84aab88b8d2b
sahithibade <SXB72020@UCMO.EDU>
2023-04-25 19:20:49 -0500

    myproject

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from view import auth_bp
 app= Flask(__name__)
 app.register_blueprint(auth_bp)
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=10000)
+    app.run(debug=True,host='0.0.0.0',port=80)
